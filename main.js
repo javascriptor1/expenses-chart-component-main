d496046c124e78a0fd18302f1e488c873d16f3c2
Mohammed Fakih <mhd.fakih@gmail.com>
2023-07-11 12:56:55 +0300

    finished chart

--- main.js
+++ main.js
@@ -1,1 +1,67 @@
 "use strict";
+
+const ctx = document.getElementById("expense-chart");
+
+/*
+  <!-- <div>mon</div>
+        <div>tue</div>
+        <div>wed</div>
+        <div>thu</div>
+        <div>fri</div>
+        <div>sat</div>
+        <div>sun</div> -->
+
+
+  */
+
+new Chart(ctx, {
+  type: "bar",
+
+  data: {
+    labels: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
+    datasets: [
+      {
+        label: "",
+        data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+        borderWidth: 1,
+        borderRadius: 5,
+        borderSkipped: false,
+        backgroundColor: [
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(186, 34%, 60%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+        ],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        display: false,
+        grid: {
+          display: false,
+        },
+      },
+      x: {
+        border: {
+          color: "transparent",
+        },
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: "hsl(28, 10%, 53%)",
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        Softred: 'hsl(10, 79%, 65%)',
+        Cyan: 'hsl(186, 34%, 60%)',
+        Darkbrown: 'hsl(25, 47%, 15%)',
+        Mediumbrown: 'hsl(28, 10%, 53%)',
+        Cream: 'hsl(27, 66%, 92%)',
+        Verypaleorange: 'hsl(33, 100%, 98%)',
+      },
+      fontFamily :{
+        sans: ['DM Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
